92703c54c7252243d3197d3e46ba60ec6989d03f
MSendraMayra <mirun2060@gmail.com>
2023-09-14 22:23:17 -0300

    Homework05

---
+++ M04 JavaScript Bucles/esVerdadero.js
@@ -0,0 +1,10 @@
+function esVerdadero(valor) {
+   // Si "valor" es verdadero retornar "Soy verdadero" .
+   // Caso contrario, retornar "Soy falso".
+   // Tu código: 
+   if (valor = true) {
+      return "Soy verdadero";
+   } else {
+      return "Soy falso";
+   }
+}
